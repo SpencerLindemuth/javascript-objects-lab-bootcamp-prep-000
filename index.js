41215d4389d00ffdc0ad01c10cc602b3f67c50f7
Spencer Lindemuth <lindemut@gmail.com>
2019-02-23 00:21:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value){
     var newObj = Object.assign({}, obj, {[key]: value})
+    return newObj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
